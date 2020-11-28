d57f035756054b9b36d1f270ba988b727c31c482
Vignesh-Viki <viki25996@gmail.com>
2020-11-28 13:28:28 +0530

    update index.js

--- index.js
+++ index.js
@@ -10,15 +10,14 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
   });
-
-app.listen(port)
+console.log(port)
+app.listen(port, () => console.log(`Running on http://localhost:${port}/`));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', router);
 
 router.post('/email', function (req, res) {
 let reqBody =req.body
-console.log(reqBody)
 let url = ''
 async function main() {
   let testAccount = await nodemailer.createTestAccount();

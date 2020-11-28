53f48320d275ee71015ea4f9a068f852a6810b09
Vignesh-Viki <viki25996@gmail.com>
2020-11-28 13:17:53 +0530

    removed nodemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express  = require('express') ,
 app =express(),
 bodyParser = require('body-parser')
-port = process.env.PORT || 800;
+port = process.env.PORT || 8000;
 const nodemailer = require("nodemailer");
 var router = express.Router();
 
